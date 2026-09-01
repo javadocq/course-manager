eeb635050c384c7b0e1a7072162fcb2b46d55342
javadocq <han18264@naver.com>
2026-09-01 17:54:18 +0900

    Feat: Course 레포지토리 및 ZOD 스키마 작성

---
+++ src/lib/courses/courses.repository.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma";
+import type { CourseInput } from "@/schemas/course.schemas";
+
+
+export const getCourse = async () => {
+  return prisma.course.findMany({
+    orderBy: {
+      createdAt: "desc",
+    }
+  })
+}
+
+export const createCourse = async (data: CourseInput) => {
+  return prisma.course.create({
+    data: {
+      title: data.title,
+      description: data.description || null,
+      instructor: data.instructor,
+      capacity: data.capacity,
+      startDate: data.startDate,
+      endDate: data.endDate,
+    },
+  });
+};
