3fb270d778f2f61bfc7fa6434516c0af4bf37c1a
javadocq <han18264@naver.com>
2026-09-02 09:10:05 +0900

    Feat: create course Service

---
+++ src/services/course.service.ts
@@ -0,0 +1,11 @@
+import { fetcher } from '@/lib/fetcher';
+import { CourseResponse, createCourseRequest } from '@/types/course';
+
+export const courseService = {
+  createCourse: (courseData: createCourseRequest) => {
+      return fetcher<CourseResponse>('/api/course', {
+        method: 'POST',
+        body: JSON.stringify(courseData),
+    });
+  }
+}
