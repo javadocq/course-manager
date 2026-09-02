eefab7d0648fcd5552cbd5ec29c76e1dd1458db8
javadocq <han18264@naver.com>
2026-09-02 09:23:25 +0900

    Feat: Query Provider 적용

---
+++ src/hooks/mutations/useCreateCourse.ts
@@ -0,0 +1,13 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { courseService } from "@/services/course.service";
+
+export const useCreateCourse = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: courseService.createCourse,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['courses'] });
+    },
+  })
+}
